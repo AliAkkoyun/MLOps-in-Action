7ccc918638e0b22de3eaa35383850287a1b0f2be
AliAkkoyun <aliakkoyun2001@gmail.com>
2025-05-19 22:51:55 +0300

    Successfuly fetch data from GCP

--- config/paths_config.py
+++ config/paths_config.py
@@ -9,3 +9,4 @@ TRAIN_FILE_PATH = os.path.join(RAW_DIR, "train.csv")
 TEST_FILE_PATH = os.path.join(RAW_DIR, "test.csv")
 
 CONFIG_PATH = "config/config.yaml"
+
--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -2,64 +2,79 @@ import os
 import pandas as pd
 from google.cloud import storage
 from sklearn.model_selection import train_test_split
-from src.custom_exception import CustomException
 from src.logger import get_logger
-from config.paths_config import *
+from src.custom_exception import CustomException
 from config.paths_config import *
 from utils.common_functions import read_yaml_file
 
 logger = get_logger(__name__)
 
 class DataIngestion:
-    def __init__(self, config):
+    def __init__(self,config):
         self.config = config["data_ingestion"]
         self.bucket_name = self.config["bucket_name"]
         self.file_name = self.config["bucket_file_name"]
         self.train_test_ratio = self.config["train_ration"]
+        
+        os.makedirs(RAW_DIR , exist_ok=True)
 
-        os.makedirs(RAW_DIR, exist_ok=True)
-        logger.info(f"Data ingestion started with {self.bucket_name} and file is {self.file_name}")
+        logger.info(f"Data Ingestion started with {self.bucket_name} and file is  {self.file_name}")
 
     def download_csv_from_gcp(self):
         try:
-            client = storage.Client()
+            json_path = "/home/ali-akkoyun/Downloads/hotel_data_private_credentials.json"
+
+            client = storage.Client.from_service_account_json(json_path)
+            logger.info("GCP client created")
             bucket = client.bucket(self.bucket_name)
             blob = bucket.blob(self.file_name)
-            blob.download_to_filename(RAW_FILE_PATH)
-            logger.info(f"File {self.file_name} downloaded from GCP bucket {self.bucket_name} to {RAW_FILE_PATH}")
-        except Exception as e:
-            logger.error(f"Error downloading file from GCP: {e}")
-            raise CustomException("Failed to download file from GCP", e)
 
+            blob.download_to_filename(RAW_FILE_PATH)
+
+            logger.info(f"CSV file is sucesfully downloaded to {RAW_FILE_PATH}")
+
+        except Exception as e:
+            logger.error("Error while downloading the csv file")
+            raise CustomException("Failed to downlaod csv file ", e)
+        
     def split_data(self):
         try:
-            logger.info("Splitting data into train and test sets")
+            logger.info("Starting the splitting process")
             data = pd.read_csv(RAW_FILE_PATH)
-            train_data, test_data = train_test_split(data, test_size=1-self.train_test_ratio, random_state=42)
+            train_data , test_data = train_test_split(data , test_size=1-self.train_test_ratio , random_state=42)
 
-            train_data.to_csv(TRAIN_FILE_PATH, index=False)
-            test_data.to_csv(TEST_FILE_PATH, index=False)
+            train_data.to_csv(TRAIN_FILE_PATH)
+            test_data.to_csv(TEST_FILE_PATH)
+
             logger.info(f"Train data saved to {TRAIN_FILE_PATH}")
             logger.info(f"Test data saved to {TEST_FILE_PATH}")
+        
         except Exception as e:
-            logger.error(f"Error splitting data: {e}")
-            raise CustomException("Failed to split data", e)
+            logger.error("Error while splitting data")
+            raise CustomException("Failed to split data into training and test sets ", e)
+        
     def run(self):
+
         try:
             logger.info("Starting data ingestion process")
+
             self.download_csv_from_gcp()
             self.split_data()
-            logger.info("Data ingestion completed successfully")
-        except Exception as e:
-            logger.error(f"Error in data ingestion: {str(e)}")
-            raise CustomException("Data ingestion failed", e)
+
+            logger.info("Data ingestion completed sucesfully")
+        
+        except CustomException as ce:
+            logger.error(f"CustomException : {str(ce)}")
+        
         finally:
-            logger.info("Data ingestion process finished")
+            logger.info("Data ingestion completed")
+
 if __name__ == "__main__":
-    try:
-        config = read_yaml_file(CONFIG_PATH)
-        data_ingestion = DataIngestion(config)
-        data_ingestion.run()
-    except Exception as e:
-        logger.error(f"Error in main: {str(e)}")
-        raise CustomException("Main function failed", e)
+    data_ingestion = DataIngestion(read_yaml_file(CONFIG_PATH))
+    data_ingestion.run()
+
+
+
+
+        
+
